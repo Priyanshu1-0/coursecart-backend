658d6566c906686890a2a0f510036dabaae5ed1f
Priyanshu1-0 <priyanshuthakkar40@gmail.com>
2025-06-26 20:56:19 -0600

    Backend Complete

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const {Router} = require("express");
 const userRouter = Router();
-const { userModel } = require("../db")
+const { userModel, purchaseModel, courseModel } = require("../db")
 const bcrypt = require("bcrypt")
 const { z } = require("zod");
 const { parse } = require("dotenv");
 const jwt = require("jsonwebtoken");
-const {JWT_USER_SECRET} = require("../config")
+const {JWT_USER_SECRET} = require("../config");
+const { userMiddleware } = require("../middleware/user");
 
 userRouter.post("/signup", async function(req,res){
     
@@ -78,9 +79,17 @@ userRouter.post("/signin", async function(req,res){
     }
 });
 
-userRouter.get("/purchases", function(req,res){
+userRouter.get("/purchases", userMiddleware, async function(req,res){
+    const userId = req.userId;
+    const purchases = await purchaseModel.find({
+        userId
+    })
+    const courseData = await courseModel.find({
+        _id: { $in: purchases.map(x => x.courseId)}
+    })
     res.json({
-        message: "Purchased Courses"
+        message: "Purchased Courses",
+        courseData
     })
 });
 
